bd9609ad0866ce9097ce415984c59d0770108ddf
udenis80 <udenis80@mail.ru>
2023-03-27 23:20:49 +0300

    add actors

--- movies/forms.py
+++ movies/forms.py
@@ -27,4 +27,27 @@ class Actors(models.Model):
         verbose_name = 'Актеры и режиссеры'
         verbose_name_plural = 'Актеры и режиссеры'
 
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField('Имя', max_length=100)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+class Movie(models.Model):
+    title = models.CharField('Название', max_length=100)
+    tagline = models.CharField('Слоган', max_length=100, default='')
+    description = models.TextField('Описание')
+    poster = models.ImageField('Постер', upload_to='movies/')
+    year = models.PositiveIntegerField('Дата выхода', default=2019)
+    country = models.CharField('Страна', max_length=30)
+    directors = models.ManyToManyField(Actor, verbose_name='режиссер', related_name='film_director')
+    actors = models.ManyToManyField(Actor, verbose_name='актеры', related_name='film_actor')
+
 
