ea6036acd3c342f135d72dd0259412e2ae04734a
udenis80 <udenis80@mail.ru>
2023-03-27 22:55:36 +0300

    start project

---
+++ movies/forms.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField('Категория', max_length=150)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Actors(models.Model):
+    """Актеры и режиссеры"""
+    name = models.CharField('Имя', max_length=100)
+    age = models.PositiveIntegerField('Возраст', default=0)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='actors/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Актеры и режиссеры'
+        verbose_name_plural = 'Актеры и режиссеры'
+
