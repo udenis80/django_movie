fc833fe33266c26f7b5a4b223d3b2bad2e0638e8
udenis80 <udenis80@mail.ru>
2023-04-05 09:40:21 +0300

    set admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots,  Actor, Rating, RatingStar, Review
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
+
+#admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre)
-admin.site.register(Movie)
+#admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Review)
+#admin.site.register(Review)
 
 
