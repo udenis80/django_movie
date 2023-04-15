ad76d7f734539dd0bd7c3f240442fbc1f9c3aea9
udenis80 <udenis80@mail.ru>
2023-04-15 19:47:17 +0300

    add email contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import Contact
 
+
 class ContactForm(forms.ModelForm):
     """Формы подписки по емейл"""
 
     class Meta:
         model = Contact
-        fields = ('email', )
+        fields = ('email',)
         widgets = {
-            'email': forms.TextInput(attrs={'class': 'editcontent'})
+            'email': forms.TextInput(attrs={'class': 'editcontent', 'placeholder': 'Your email...'})
+        }
+        labels = {
+            'email': ''
         }
--- django_movie/settings.py
+++ django_movie/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
      'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,6 +121,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+gettext = lambda s: s
+LANGUAGES = (
+    ('ru', gettext('Russia')),
+    ('en', gettext('English')),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
